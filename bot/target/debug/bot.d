ef760338fb93696c5e29ac4e3779451c5f64284e
jpnykw <jpnykw@gmail.com>
2021-06-29 20:25:36 +0900

    :package: Setup with cargo

---
+++ bot/target/debug/bot.d
@@ -0,0 +1,1 @@
+/Users/harutohirakawa/Desktop/Devs/shinchoku-chan/bot/target/debug/bot: /Users/harutohirakawa/Desktop/Devs/shinchoku-chan/bot/src/main.rs
---
+++ bot/target/debug/deps/bot.d
@@ -0,0 +1,5 @@
+/Users/harutohirakawa/Desktop/Devs/shinchoku-chan/bot/target/debug/deps/bot: src/main.rs
+
+/Users/harutohirakawa/Desktop/Devs/shinchoku-chan/bot/target/debug/deps/bot.d: src/main.rs
+
+src/main.rs:
