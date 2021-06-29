5eec6ae7142058a4edfd54f0880ab40b8d8290d5
jpnykw <jpnykw@gmail.com>
2021-06-30 03:05:38 +0900

    :tada: Implement the connection to DB

--- bot/src/commands/record.rs
+++ bot/src/commands/record.rs
@@ -1,10 +1,22 @@
+use std::env;
+
 use serenity::{
     prelude::*,
     model::prelude::*,
 };
 
+use diesel::mysql::MysqlConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+
+fn establish_connection() -> MysqlConnection {
+    let database_url = env::var("DB_URL").expect("DB_URL is not found in enviroment variable!");
+    MysqlConnection::establish(&database_url).expect(&format!("Failed connecting to {}", database_url))
+}
+
 // TODO: 受け取った内容をパースしてDBに記録する
 pub fn record(ctx: &Context, msg: &Message) {
-    msg.channel_id.say(ctx, "まだできないよ");
+    let connection: MysqlConnection = establish_connection();
+    msg.channel_id.say(ctx, "DBへの接続に成功したよ");
 }
 
