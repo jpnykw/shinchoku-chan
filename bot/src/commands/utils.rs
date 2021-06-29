ac6c394f69f91f736c39fcd342a268a7f7604267
jpnykw <jpnykw@gmail.com>
2021-06-29 21:15:32 +0900

    :recycle: Define commands in external files

---
+++ bot/src/commands/utils.rs
@@ -0,0 +1,8 @@
+use serenity::{
+    prelude::*,
+    model::prelude::*,
+};
+
+pub fn ping(ctx: &Context, msg: &Message) {
+    msg.channel_id.say(ctx, "Pong!");
+}
--- bot/src/main.rs
+++ bot/src/main.rs
@@ -4,6 +4,11 @@ use serenity::{
     prelude::*,
 };
 
+mod commands;
+use commands::{
+    utils::*,
+};
+
 struct Handler {
     prefix: String
 }
@@ -18,11 +23,7 @@ impl EventHandler for Handler {
             let mut args = tokens.collect::<Vec<&str>>();
 
             match command {
-                "ping" => {
-                    if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
-                        println!("Error sending message: {:?}", why);
-                    }
-                },
+                "ping" => ping(&ctx, &msg),
                 _ => {
                     if let Err(why) = msg.channel_id.say(&ctx.http, &format!("Unknown command `{}`", command)) {
                         println!("Error sending message: {:?}", why);
@@ -38,8 +39,9 @@ impl EventHandler for Handler {
 }
 
 fn main() {
+    let header = Handler{ prefix: "<@!859396354328756255>".to_string() };
     let token = env::var("DISCORD_BOT_TOKEN").expect("Token is not found in enviroment variable!");
-    let mut client = Client::new(&token, Handler{ prefix: "!".to_string() }).expect("Err creating client");
+    let mut client = Client::new(&token, header).expect("Err creating client");
     if let Err(why) = client.start() {
         println!("Error start client: {:?}", why);
     }
