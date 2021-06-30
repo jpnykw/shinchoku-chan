c3ffdc3fda227d57f7092edcabdfbbef6f9ac06a
jpnykw <jpnykw@gmail.com>
2021-07-01 08:24:16 +0900

    :sparkles: Implement command to get the status of Heroku

--- bot/src/commands/utils.rs
+++ bot/src/commands/utils.rs
@@ -7,3 +7,15 @@ pub fn ping(ctx: &Context, msg: &Message) {
     msg.channel_id.say(ctx, "ぽん！");
 }
 
+pub fn heroku_ps(ctx: &Context, msg: &Message) {
+    use std::process::Command;
+    msg.channel_id.say(ctx, "ちょっとだけ待ってね…");
+
+    let output = Command::new("heroku")
+        .args(&["ps"])
+        .output()
+        .expect("Failed to execute `heroku ps`");
+
+    let result = String::from_utf8_lossy(&output.stdout);
+    msg.channel_id.say(ctx, &format!("heroku の様子だよ！\n```r\n{}\n```", result));
+}
