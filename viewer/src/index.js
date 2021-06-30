aacf1ba3aaa0fef3658a174ec8c01cfac77b7681
jpnykw <jpnykw@gmail.com>
2021-07-01 03:09:06 +0900

    :sparkles: Implement method to connect to heroku's database

--- viewer/src/index.js
+++ viewer/src/index.js
@@ -1,10 +1,21 @@
+const password = process.env.PASSWORD;
+const port = process.env.PORT || 8080;
+
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('OK');
+const connection = mysql.createConnection({
+  host: 'us-cdbr-east-04.cleardb.com',
+  user: 'b529d6a4aa7138',
+  database: 'heroku_29a6a27e7069b46',
+  password,
 });
 
-const port = process.env.PORT || 8080;
+app.get('/', (_, response) => {
+  connection.query('SELECT * FROM posts', (_, result) => {
+    response.send(`OK:<br>${result}`);
+  });
+});
+
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
