602499945e0184c350561dde5b62569232b52e96
jpnykw <jpnykw@gmail.com>
2021-07-02 01:21:55 +0900

    :construction: Generate mysql query as dynamic

--- viewer/backend/index.js
+++ viewer/backend/index.js
@@ -18,10 +18,54 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// テーブルの値を全て取得するためのエンドポイント
-app.get('/api', (_, response) => {
+// テーブルの値を取得するためのエンドポイント
+app.get('/api', (request, response) => {
+  const keys = Object.keys(request.query);
+  const mode = 'OR';
+  let data = null;
+
+  const query = {
+    mysql: 'SELECT * FROM posts',
+    where: false,
+    limit: false,
+    order_by: false,
+  };
+
+  if (keys.length > 0) {
+    for (const key of keys) {
+      switch (key) {
+        default:
+          // １つのカラムに複数の値が存在する場合は IN を用いる
+          // 例 ?name=neko -> [neko]
+          // 例 ?name=neko,inu -> [neko, inu]
+          data = request.query[key].split(',').map((data) => `'${data}'`);
+
+          if (data.length === 1) {
+            query.mysql = `${query.mysql} ${query.where ? mode : 'WHERE'} ${key} = ${data[0]}`;
+          } else {
+            query.mysql = `${query.mysql} ${query.where ? mode : 'WHERE'} ${key} in (${data.join(', ')})`;
+          }
+
+          query.where = true;
+          break;
+
+        case 'order':
+          // 対象のカラム、ソート順、の順番で受け取る
+          // 例 ?order=name,desc
+          data = request.query[key].split(',');
+          query.mysql = `${query.mysql} ORDER BY ${data[0]} ${data[1] === 'desc' ? 'DESC' : 'ASC'}`;
+          break;
+      }
+    }
+  }
+
   pool.getConnection((_, connection) => {
-    connection.query('SELECT * FROM posts ORDER BY date DESC LIMIT 50', (_, result) => {
+    connection.query(query.mysql, (error, result) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
       response.json({ result: JSON.stringify(result) });
     });
   });
