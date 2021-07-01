31f2012b263fbd54e2b7b68ca054d91c3c9cc4ea
jpnykw <jpnykw@gmail.com>
2021-07-01 23:45:44 +0900

    :tada: Create UI for frontend using Material-UI

--- viewer/frontend/src/components/Main.js
+++ viewer/frontend/src/components/Main.js
@@ -1,4 +1,35 @@
 import {useState, useEffect} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+// import { Button, Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  table: {
+    minWidth: 650,
+  },
+  title: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(4),
+  },
+  tableGrid: {
+    marginTop: theme.spacing(4),
+  },
+}));
 
 const Main = () => {
   const [progress, setProgress] = useState('');
@@ -12,34 +43,46 @@ const Main = () => {
   useEffect(() => {
     if (progress === '') return false;
     console.log(JSON.parse(progress));
-  })
+  });
+
+  const classes = useStyles();
 
   return (
-    <div className="App">
-      <h1>💪進捗リスト💪</h1>
-      <button onClick={fetch_posts_from_db}>データを取得する</button>
-      <table>
-        <tbody>
-          <tr>
-            <th>ユーザー</th>
-            <th>日時</th>
-            <th>内容</th>
-          </tr>
-          {
-            (progress === '' ? [] : JSON.parse(progress)).map((row_data_packet, key) => {
+    <>
+      <Container>
+        <Typography variant="h4" className={classes.title}>💪進捗リスト💪</Typography>
+        <Button variant="outlined" color="secondary" onClick={fetch_posts_from_db}>データを取得する</Button>
+      </Container>
+
+      {/* <Container className={classes.tableContainer}> */}
+      <Grid container justify = "center" className={classes.tableGrid}>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ユーザー</TableCell>
+                <TableCell>投稿日時</TableCell>
+                <TableCell>投稿内容</TableCell>
+              </TableRow>
+            </TableHead>
+
+          <TableBody>
+            {(progress === '' ? [] : JSON.parse(progress)).map((row_data_packet, key) => {
               const { name, content, date } = row_data_packet;
               return (
-                <tr key={key}>
-                  <td>{ name }</td>
-                  <td>{ date }</td>
-                  <td>{ content }</td>
-                </tr>
+                <TableRow key={key}>
+                  <TableCell>{ name }</TableCell>
+                  <TableCell>{ date }</TableCell>
+                  <TableCell>{ content }</TableCell>
+                </TableRow>
               )
-            })
-          }
-        </tbody>
-      </table>
-    </div>
+            })}
+          </TableBody>
+          </Table>
+        </TableContainer>
+      {/* </Container> */}
+      </Grid>
+    </>
   );
 }
 
