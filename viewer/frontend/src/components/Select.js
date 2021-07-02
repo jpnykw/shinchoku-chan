523b39844fd6973b72465f21d0d565db2e0d7958
jpnykw <jpnykw@gmail.com>
2021-07-02 18:30:29 +0900

    :art: Wrap the component Select

---
+++ viewer/frontend/src/components/Select.js
@@ -0,0 +1,9 @@
+import { default as MaterialSelect } from '@material-ui/core/Select';
+
+const Select = (props) => {
+  return (
+    <MaterialSelect {...props} />
+  );
+};
+
+export default Select;
