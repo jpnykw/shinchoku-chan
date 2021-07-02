2efb55af561cd90949c5bb6f2434fef5b8e1f55c
jpnykw <jpnykw@gmail.com>
2021-07-02 18:14:22 +0900

    :art: Wrap the component Container

---
+++ viewer/frontend/src/components/Container.js
@@ -0,0 +1,9 @@
+import { default as MaterialContainer } from '@material-ui/core/Container';
+
+const Container = (props) => {
+  return (
+    <MaterialContainer {...props} />
+  );
+};
+
+export default Container;
