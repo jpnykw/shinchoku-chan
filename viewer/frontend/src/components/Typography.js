8d4cc07199be03765e3ee0a3d1689609ea72a814
jpnykw <jpnykw@gmail.com>
2021-07-02 18:07:14 +0900

    :art: Wrap the component Typography

---
+++ viewer/frontend/src/components/Typography.js
@@ -0,0 +1,9 @@
+import { default as MaterialTypography } from '@material-ui/core/Typography';
+
+const Typography = (props) => {
+  return (
+    <MaterialTypography {...props} />
+  );
+};
+
+export default Typography;
