2c02a8e27e3d6eaecfc5e4e586b1f737b9a293ac
jpnykw <jpnykw@gmail.com>
2021-07-02 18:02:30 +0900

    :art: Wrap the component Button

--- viewer/frontend/src/index.js
+++ viewer/frontend/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import Main from './components/Main';
+import Main from './pages/Main';
 import './index.css';
 
-ReactDOM.render(
-  <>
-    <Main />
-  </>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Main />, document.querySelector('#root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
