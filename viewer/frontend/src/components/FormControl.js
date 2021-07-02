1e50943db08789cde5084ce75dd74004579071cc
jpnykw <jpnykw@gmail.com>
2021-07-02 18:27:45 +0900

    :art: Wrap the component FormControl

---
+++ viewer/frontend/src/components/FormControl.js
@@ -0,0 +1,9 @@
+import { default as MaterialFormControl } from '@material-ui/core/FormControl';
+
+const FormControl = (props) => {
+  return (
+    <MaterialFormControl {...props} />
+  );
+};
+
+export default FormControl;
