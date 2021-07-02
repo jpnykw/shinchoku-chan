b64d9bbecafff3f38fdb002fbf513a19bdb77d05
jpnykw <jpnykw@gmail.com>
2021-07-03 02:13:48 +0900

    :sparkles: Implement UTC to JST timestamp conversion

--- viewer/frontend/src/pages/Main.js
+++ viewer/frontend/src/pages/Main.js
@@ -17,6 +17,36 @@ import TableRow from '../components/TableRow.js';
 import TextField from '../components/TextField.js';
 import Typography from '../components/Typography.js';
 
+const zero_pad = (...args) => {
+  return args.map(number => {
+    if (number.length > 1 && number[0] === '0') return number;
+    return `${Number(number) < 10 ? '0' : ''}${number}`;
+  });
+}
+
+const parse_date = (date_string) => {
+  let date = date_string.split(' ')[0];
+  let time = date_string.split(' ')[1];
+
+  const [year, month, day] = date.split('/');
+  date = zero_pad(year, month, day).join('/');
+  const [hour, minute, second] = time.split(':');
+  time = zero_pad(hour, minute, second).join(':');
+  return `${date} ${time}`;
+}
+
+const zero_pad_date = (date_string = null) => {
+  if (date_string === null) return null;
+  return parse_date(date_string);
+}
+
+const utc_to_jst = (utc) => {
+  const date = new Date(utc);
+  date.setHours(date.getHours() + 9);
+  const result = zero_pad_date(date.toLocaleString('ja'));
+  return result === null ? 'タイムスタンプが不正な値です' : result;
+}
+
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
@@ -184,7 +214,7 @@ const Main = () => {
                   <TableHead>
                     <TableRow>
                       <TableCell>ユーザー</TableCell>
-                      <TableCell>投稿日時(UTC)</TableCell>
+                      <TableCell>投稿日時(JST)</TableCell>
                       <TableCell>投稿内容</TableCell>
                     </TableRow>
                   </TableHead>
@@ -194,7 +224,7 @@ const Main = () => {
                       return (
                         <TableRow key={key}>
                           <TableCell>{ name }</TableCell>
-                          <TableCell>{ date }</TableCell>
+                          <TableCell>{ utc_to_jst(date) }</TableCell>
                           <TableCell>{ content }</TableCell>
                         </TableRow>
                       );
