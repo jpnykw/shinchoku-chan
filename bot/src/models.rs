5b90c2c854098f5b55211ad5b7f7993dfc56f4dc
jpnykw <jpnykw@gmail.com>
2021-07-05 17:25:46 +0900

    :green_heart: Create new models for new table

--- bot/src/models.rs
+++ bot/src/models.rs
@@ -1,6 +1,8 @@
 use crate::schema::posts;
+use crate::schema::commits;
 use chrono::naive::NaiveDateTime;
 
+// 進捗や記録を保存するためのテーブル
 #[derive(Queryable, Debug)]
 pub struct Post {
     pub id: i32,
@@ -14,6 +16,31 @@ pub struct Post {
 pub struct NewPost<'a> {
     pub name: &'a str,
     pub content: &'a str,
+    pub date: NaiveDateTime,
+}
+
+// commit の変更料 (差分) を保存するためのテーブル
+#[derive(Queryable, Debug)]
+pub struct Commit {
+    pub id: i32,
+    pub user: String,
+    pub additions: i32,
+    pub deletions: i32,
+    pub hash: String,
+    pub repo_owner: String,
+    pub repo_name: String,
+    pub date: Option<NaiveDateTime>,
+}
+
+#[derive(Insertable)]
+#[table_name = "commits"]
+pub struct NewCommit<'a> {
+    pub user: &'a str,
+    pub additions: &'a i32,
+    pub deletions: &'a i32,
+    pub hash: &'a str,
+    pub repo_owner: &'a str,
+    pub repo_name: &'a str,
     pub date: NaiveDateTime,
 }
 
