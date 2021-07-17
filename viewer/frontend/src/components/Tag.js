832481ca8c277ba593bd798bf5c383d339a3d732
jpnykw <jpnykw@gmail.com>
2021-07-18 03:57:33 +0900

    :tada: Implement feature to show the data with Tag as table mode

---
+++ viewer/frontend/src/components/Tag.js
@@ -0,0 +1,9 @@
+import Chip from '@material-ui/core/Chip';
+
+const Tag = (props) => {
+  return (
+    <Chip {...props} />
+  );
+};
+
+export default Tag;
--- viewer/frontend/src/pages/TableView.js
+++ viewer/frontend/src/pages/TableView.js
@@ -7,6 +7,7 @@ import TableCell from '../components/TableCell.js';
 import TableContainer from '../components/TableContainer.js';
 import TableHead from '../components/TableHead.js';
 import TableRow from '../components/TableRow.js';
+import Tag from '../components/Tag.js';
 
 import { utc_to_jst } from '../utils/date.js';
 
@@ -35,13 +36,16 @@ const TableView = ({
           <TableHead>
             <TableRow>
               <TableCell>ユーザー</TableCell>
+              <TableCell>タグ</TableCell>
               <TableCell>投稿日時(JST)</TableCell>
               <TableCell>投稿内容</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {JSON.parse(progress).map((row_data_packet, key) => {
-              const { name, content, date } = row_data_packet;
+              const { name, content, date, tag } = row_data_packet;
+              const tag_key = ['progress', 'record', 'impression'][['進捗', '記録', '感想'].indexOf(tag === '' ? '進捗' : tag)];
+
               return (
                 <TableRow
                   style={
@@ -57,6 +61,26 @@ const TableView = ({
                   key={key}
                 >
                   <TableCell>{ name }</TableCell>
+                  <TableCell>
+                    <Tag
+                      size={'small'}
+                      variant={'outlined'}
+                      label={
+                        {
+                          'record': '記録',
+                          'impression': '感想',
+                          'progress': '進捗',
+                        }[tag_key]
+                      }
+                      color={
+                        {
+                          'record': 'default',
+                          'impression': 'primary',
+                          'progress': 'secondary',
+                        }[tag_key]
+                      }
+                    />
+                  </TableCell>
                   <TableCell>{ utc_to_jst(date) }</TableCell>
                   <TableCell>{ content }</TableCell>
                 </TableRow>
